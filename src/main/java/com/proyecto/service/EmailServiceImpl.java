e65a8312baa5bfa0313179a7f7e7ecef1ba10b76
pgarcia85 <villoriaponton@gmail.com>
2021-05-28 16:24:48 +0200

    Modificacion memoria y nuevas carpetas en proyecto

--- src/main/java/com/proyecto/service/EmailServiceImpl.java
+++ src/main/java/com/proyecto/service/EmailServiceImpl.java
@@ -1,7 +1,6 @@
 package com.proyecto.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
