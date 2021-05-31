e3706d3a9abdddd8e7ae2af1a4990aa62e82e24f
pgarcia85 <villoriaponton@gmail.com>
2021-05-31 18:03:18 +0200

    cambios para generar el war

--- src/main/java/com/proyecto/PaulaGarciaSuarez_PROYECTO.java
+++ src/main/java/com/proyecto/PaulaGarciaSuarez_PROYECTO.java
@@ -3,9 +3,16 @@ package com.proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PaulaGarciaSuarez_PROYECTO {
+public class PaulaGarciaSuarez_PROYECTO extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PaulaGarciaSuarez_PROYECTO.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaulaGarciaSuarez_PROYECTO.class, args);
