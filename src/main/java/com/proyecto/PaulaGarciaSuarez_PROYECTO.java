dcfd627dd4733a45587d43bcdd7331073e8cbc24
pgarcia85 <villoriaponton@gmail.com>
2021-06-02 18:53:27 +0200

    Revert "cambios para generar el war"
    This reverts commit e3706d3a9abdddd8e7ae2af1a4990aa62e82e24f.

--- src/main/java/com/proyecto/PaulaGarciaSuarez_PROYECTO.java
+++ src/main/java/com/proyecto/PaulaGarciaSuarez_PROYECTO.java
@@ -3,16 +3,9 @@ package com.proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PaulaGarciaSuarez_PROYECTO extends SpringBootServletInitializer{
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(PaulaGarciaSuarez_PROYECTO.class);
-	}
+public class PaulaGarciaSuarez_PROYECTO {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaulaGarciaSuarez_PROYECTO.class, args);
