a011c1a392e0bde6b04f6e829b472f3cbccea740
pgarcia85 <villoriaponton@gmail.com>
2021-06-03 19:32:18 +0200

    se corrige consulta

--- src/main/java/com/proyecto/repository/UsuarioPostRepository.java
+++ src/main/java/com/proyecto/repository/UsuarioPostRepository.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-
 import org.springframework.transaction.annotation.Transactional;
 
 import com.proyecto.model.UsuarioPostFavoritoVO;
@@ -15,7 +13,7 @@ public interface UsuarioPostRepository extends CrudRepository<UsuarioPostFavorit
 	
 	List<UsuarioPostFavoritoVO> findAllByUsuarioFavIdusuarioOrderByPostFavFechacreacionDesc(Long idUsuario);
 	
-
+	@Transactional
 	long deleteByUsuarioFavIdusuarioAndPostFavIdpost(Long idUsuario, Integer idPost);
 
 }
